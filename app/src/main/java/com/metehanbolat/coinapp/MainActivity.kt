a753ebed8ba961f64d9f57839fe1f3713ac8aca4
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-02-22 17:27:20 +0300

    Initial commit

---
+++ app/src/main/java/com/metehanbolat/coinapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.metehanbolat.coinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.metehanbolat.coinapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        
+    }
+}
